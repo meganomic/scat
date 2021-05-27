3cbb7756b51706bcfa420d9dda0b4889c85982bf
meganomic <>
2021-05-27 16:44:51 +0200

    More optimizations

--- scat.asm
+++ scat.asm
@@ -4,37 +4,42 @@ default rel
 
 ; ELF64 Header
 ehdr:
-                db 0x7F, "ELF", 2, 1, 1, 0 ; e_ident[16]
+                db 0x7F, "ELF", 2;, 1, 1, 0 ; e_ident[16]
                 ;dq 0 ;
+
 _start:
     ; This is where the program starts
     ; Compare Argc against 2, the first argv is the path to the binary
     ; So 2 means the program was given 1 argument
     pop rax ; 1 byte
-    pop rdi ; 1 byte
     cmp al, 2 ; 2 bytes
-    je short openfile ; 2 bytes
-    jmp short perr ; 2 bytes
+    jne short perr ; 2 bytes
+    pop rdi ; 1 byte
+    pop rdi ; 1 byte
+    syscall ; 2 bytes
+    jmp short openfile1 ; 2 bytes
 
                 dw 2 ; e_type
                 dw 62 ; e_machine
-                dd 1 ; e_version
+
+openfile1:
+    test eax, eax ; 2 bytes
+    jmp short openfile2 ; 2 bytes
+                ;dd 1 ; e_version
                 dq _start; e_entry      /* Entry point virtual address */
                 dq phdr - $$; e_phoff   /* Program header table file offset */
-openfile:
+
+openfile2:
     ; Load the pointer to the 1st 'real' argument aka the filepath into rdi and open it
     ; rax is already 2 here which is sys_open
-    pop rdi ; 1 byte - pointer to the path to the file to open
-    syscall ; 2 bytes
-    test eax, eax ; 2 bytes
     mov bl, 5 ; 2 bytes - This is put in rax later for the fstat call
 perr:
     push byte 2 ; 2 bytes
     pop rdi ; 1 byte
     js short exit ; 2 bytes
+    mov esi, buffer ; 5 bytes
     jmp short fstathdr ; 2 bytes
 
-                ;db 0 ; filler byte
                 ;dq 0 ; e_shoff          /* Section header table file offset */
                 ;dd 0 ; e_flags
                 ;dw 64 ; e_ehsize
@@ -47,13 +52,21 @@ perr:
 
 phdr:
                 dd 1 ; p_type;
-                dd 7 ; p_flags;
+                ;dd 7 ; p_flags;
+
+fstat1:
+    inc edi
+    jmp short fstat
+
                 dq 0 ; p_offset;                      /* Segment file offset */
                 dq $$ ; p_vaddr;                      /* Segment virtual address */
+
 fstathdr:
     xchg ebx, eax ; 1 byte - Save FD to ebx and set eax to zero. ebx is zero here
-    mov esi, buffer ; 5 bytes - pointer to the buffer
-    jmp short fstat ; 2 bytes
+    mov edi, ebx ; 2 bytes
+    syscall ; 2 bytes
+    jmp short fstat1 ; 2 bytes
+    db 0 ; filler byte
 
                 ;dq 0 ; p_paddr;                       /* Segment physical address */
                 dq end_of_code-$$ ; p_filesz          /* Segment size in file */
@@ -62,12 +75,6 @@ fstathdr:
 
 
 fstat:
-    ; Fstat call to get file size
-    mov edi, ebx ; fd
-    syscall
-
-    inc edi ; Set exit code
-
     test eax, eax
     js short exit
 
@@ -77,7 +84,7 @@ fstat:
 read_loop:
     ; rax is already zero here which is sys_read
     mov edi, ebx ; fd
-    mov dx, 65535 ; count
+    mov edx, ecx ; count - RCX contains the return address of the last system call. Aka 0x04000xx
     syscall
 
     test eax, eax
@@ -99,7 +106,7 @@ write:
     sub r13, rdx
     jnz short read_loop
 
-    xor edi, edi ; Set exit code to 0
+    pop rdi ; Set RDI to zero. There's one on the stack, use that
 
 
 exit:
@@ -110,6 +117,6 @@ exit:
 end_of_code:
 
 section .bss
-buffer: resb 65535
+buffer: resb 0x400100
 
 end_of_bss:
